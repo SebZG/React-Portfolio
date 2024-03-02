5685c625987478205f4de5e4c4733206ee6dcae1
SebZG <sebastian.zapata.g@gmail.com>
2024-03-02 19:25:16 +0000

    Added pages/components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import Projects from './pages/Projects/Projects';
+import Contact from './pages/Contact/Contact';
 
 const App = () => {
 
 	return (
-		<>
-			<h1 className="">
-				Hello world!
-			</h1>
-		</>
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+		</Router>
 	)
 }
 
